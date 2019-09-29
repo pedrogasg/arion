2f2a990f5b3149d76ba1c7349eb2a1d20e98fd8f
FILLASTRE Pedro <pedro.fillastre@renault.com>
2019-09-30 01:10:47 +0200

    Adding launch with static transform and viz

--- src/arion/node/odometry_node.py
+++ src/arion/node/odometry_node.py
@@ -5,17 +5,21 @@ from nav_msgs.msg import Odometry
 class OdometryNode:
     def __init__(self):
         rospy.init_node('odometry_test', anonymous=True, log_level= rospy.INFO)
-        self.odometer = VisionOdometer()
-        self.pub = rospy.Publisher('/mavros/odometry/out', Odometry, queue_size=1)
+
+        topic_in = rospy.get_param('~input_topic', '/camera/odom/sample')
+        topic_out = rospy.get_param('~output_topic', '/mavros/odometry/out')
+        
+        self.rate= rospy.get_param('~output_rate', 30)
+
+        self.odometer = VisionOdometer(topic_in)
+        self.pub = rospy.Publisher(topic_out, Odometry, queue_size=1)
 
 
     def run(self):
-        r =rospy.Rate(30)
+        r =rospy.Rate(self.rate)
 
         while not rospy.is_shutdown():
             odom = self.odometer.read()
             if odom is not None:
-                rospy.loginfo(odom.header.frame_id)
-                rospy.loginfo(odom.child_frame_id)
                 self.pub.publish(odom)
             r.sleep()
--- src/arion/position.py
+++ src/arion/position.py
@@ -4,10 +4,10 @@ from copy import deepcopy
 from nav_msgs.msg import Odometry
 
 class VisionOdometer:
-    def __init__(self):
+    def __init__(self, topic):
         self.read_lock = threading.Lock()
         self.odom = Odometry()
-        self.odom_sub = rospy.Subscriber('/camera/odom/sample', Odometry, self.receive_pose)
+        self.odom_sub = rospy.Subscriber(topic, Odometry, self.receive_pose)
 
     def is_started(self):
         return self.odom is not None
@@ -21,8 +21,6 @@ class VisionOdometer:
             odom = deepcopy(self.odom)
         return odom
 
-
-        
     
 
 
