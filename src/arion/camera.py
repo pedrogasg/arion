c3594d55074d58c502a7f333b9ca1f6e8675e414
FILLASTRE Pedro <pedro.fillastre@renault.com>
2019-10-11 00:53:15 +0200

    Changing for traitlets

--- src/arion/camera.py
+++ src/arion/camera.py
@@ -1,62 +1,75 @@
-#!/usr/bin/env python3
 import cv2
+import atexit
+import traitlets
 import threading
+import numpy as np
 
 
-class GStreamerCamera:
-    _DEFAULT_RATE_ = 120
-    _DEFAULT_WIDTH_ = 1280
-    _DEFAULT_HEIGHT_ = 720
 
-    def __init__(self, src=0, width=410, height=308):
-        self.src = src
-        self.width = width
-        self.height = height
-        self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
-        self.grabbed, self.frame = self.cap.read()
-        self.started = False
-        self.read_lock = threading.Lock()
-        self.center = (width / 2, height / 2)
-        self.M = cv2.getRotationMatrix2D(self.center, 180, 1.0)
+class GStreamerCamera(traitlets.HasTraits):
 
-    def set(self, var1, var2):
-        self.cap.set(var1, var2)
-    
-    def _gst_str(self):
-        return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d,format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv ! video/x-raw,  width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert  ! video/x-raw, format=(string)BGR ! appsink' % (
-                self.src, self._DEFAULT_WIDTH_, self._DEFAULT_HEIGHT_, self._DEFAULT_RATE_, self.width, self.height)
+    value = traitlets.Any()
+    width = traitlets.Integer(default_value=320)
+    height = traitlets.Integer(default_value=180)
+    running = traitlets.Bool(default_value=False)
+    src = traitlets.Integer(default_value=0)
+    rate = traitlets.Integer(default_value=30)
+    native_width = traitlets.Integer(default_value=1280)
+    native_height = traitlets.Integer(default_value=720)
 
     @staticmethod
     def encode_image(image):
-         return cv2.imencode('.jpg', image)
+        return bytes(cv2.imencode('.jpg', value)[1])
+    
+    def __init__(self, *args, **kwargs):
+        super(GStreamerCamera, self).__init__(*args, **kwargs)
 
-    def start(self):
-        if self.started:
-            rospy.loginfo('[!] Asynchroneous video capturing has already been started.')
-            return None
-        self.started = True
-        self.thread = threading.Thread(target=self.update, args=())
-        self.thread.start()
+        self.value = np.empty((self.height, self.width, 3), dtype=np.uint8)
+        self._running = False
+        try:
+            self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
 
-    def update(self):
-        while self.started:
-            grabbed, frame = self.cap.read()
-            with self.read_lock:
-                self.grabbed = grabbed
-                self.frame = frame
+            re, image = self.cap.read()
 
+            if not re:
+                raise RuntimeError('Could not read image from camera.')
+        except:
+            raise RuntimeError(
+                'Could not initialize camera.  Please see error trace.')
+
+        atexit.register(self.cap.release)
+
+    def _gst_str(self):
+        return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
+                self.src, self.native_width, self.native_height, self.rate, self.width, self.height)
+    
+    def _read(self):
+        re, image = self.cap.read()
+        if re:
+            return image
+        else:
+            raise RuntimeError('Could not read image from camera')
+        
     def read(self):
-        with self.read_lock:
-            frame = self.frame.copy()
-            grabbed = self.grabbed
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #invert image cv2.warpAffine(frame, self.M, (self.width, self.height))
-        return grabbed, frame
-
-    def stop(self):
-        self.started = False
-        self.thread.join()
-
-    def __exit__(self, exec_type, exc_value, traceback):
-        self.stop()
-        self.cap.release()
+        if self._running:
+            raise RuntimeError('Cannot read directly while camera is running')
+        self.value = self._read()
+        return self.value
+    
+    def _capture_frames(self):
+        while True:
+            if not self._running:
+                break
+            self.value = self._read()
+            
+    @traitlets.observe('running')
+    def _on_running(self, change):
+        if change['new'] and not change['old']:
+            # transition from not running -> running
+            self._running = True
+            self.thread = threading.Thread(target=self._capture_frames)
+            self.thread.start()
+        elif change['old'] and not change['new']:
+            # transition from running -> not running
+            self._running = False
+            self.thread.join()
--- src/arion/node/camera_node.py
+++ src/arion/node/camera_node.py
@@ -9,26 +9,22 @@ class CameraNode:
 
         rospy.init_node('camera_arion', anonymous=True, log_level= rospy.INFO)
         topic_out = rospy.get_param('~camera_out_topic', '/arion/image_compressed')
-        width = rospy.get_param('~camera_witdh', 410)
-        height = rospy.get_param('~camera_height', 308)
+        width = rospy.get_param('~camera_witdh', 320)
+        height = rospy.get_param('~camera_height', 180)
         self.rate = rospy.get_param('~camera_rate', 30)
-        self.cam = GStreamerCamera(src=0, width=width, height=height)
+        self.cam = GStreamerCamera(src=0, width=width, height=height, rate=rate)
         self.pub = rospy.Publisher(topic_out, CompressedImage, queue_size=1)
+
+    def update_image(self, change):
+        msg = CompressedImage()
+        msg.header.stamp = rospy.Time.now()
+        msg.format = "jpeg"
+        msg.data = GStreamerCamera.encode_image(change['new'])
+        self.pub.publish(msg)
 
 
     def run(self):
-        r =rospy.Rate(self.rate)
-        self.cam.start()
-        while not rospy.is_shutdown():
-            _, frame = self.cam.read()
-
-            #### Create CompressedIamge ####
-            msg = CompressedImage()
-            msg.header.stamp = rospy.Time.now()
-            msg.format = "jpeg"
-            msg.data = np.array(GStreamerCamera.encode_image(frame)[1]).tostring()
-
-           # Publish new image
-            self.pub.publish(msg)
-            r.sleep()
-        self.cam.stop()
+        self.cam.running = True
+        self.cam.observe(self.update_image, names='value')
+        rospy.spin()
+        self.cam.running = False
