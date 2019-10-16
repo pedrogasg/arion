d34d76772b8fcc97f122d5ff736a877b225a59c1
FILLASTRE Pedro <pedro.fillastre@renault.com>
2019-10-16 11:02:59 +0200

    test

--- src/arion/offboard.py
+++ src/arion/offboard.py
@@ -1,4 +1,5 @@
 import rospy
+import time
 from mavros_msgs.srv import CommandBool, SetMode
 
 class OffboardControl:
@@ -15,10 +16,10 @@ class OffboardControl:
 
     def set_mode(self, mode="MANUAL"):
         if self.flightModeService(custom_mode=mode):
-            rospy.loginfo("Vehicle is now in %s mode" %mode)
+            rospy.loginfo("Vehicle is now in %s mode" % mode)
             return True
         else:
-            rospy.loginfo("Vehicle %s failed" %mode)
+            rospy.loginfo("Vehicle %s failed" % mode)
             return False
 
     def arm(self):
@@ -38,12 +39,11 @@ class OffboardControl:
             return False
     
     def take_control(self, callback):
-        callback()
-        callback()
-        callback()
-        callback()
-        while not self.arm() and not self.offboard():
+        for i in range(10):
             callback()
+            self.arm()
+            self.offboard()
+            rospy.loginfo("%d" % i)
             time.sleep(0.2)
 
     def release_control(self, callback):
