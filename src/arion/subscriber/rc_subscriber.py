cb1364d615426c222291363ebbf72971a7f42711
FILLASTRE Pedro <pedro.fillastre@renault.com>
2019-10-27 03:52:22 +0100

    Fix

--- src/arion/subscriber/rc_subscriber.py
+++ src/arion/subscriber/rc_subscriber.py
@@ -7,7 +7,7 @@ class RCSubscriber:
     _ZERO_ = 1500
     def start_rc(self):
         self.channels = np.ones(RCSubscriber._ACTUATORS_) * RCSubscriber._ZERO_
-        self.rc_subscriber = rospy.Subscriber("mavros/rc/in", RCIn, self.update_speed, queue_size=1)
+        self.rc_subscriber = rospy.Subscriber("mavros/rc/in", RCIn, self.update_rc, queue_size=1)
 
     def update_rc(self, rc):
         self.channels = rc.channels
